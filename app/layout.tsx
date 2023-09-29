749d179082405d2a857bf8be61dbd205540ac202
Qavi-Nizamani <qaviniz11@gmail.com>
2023-09-29 16:59:37 +0500

    add assets, meta and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,40 @@
-import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next'
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google'
 
-export const metadata = {
-  title: 'Next.js 13 with Clerk'
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: [
+    '100',
+    '200',
+    '300',
+    '400',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900'
+  ],
+  variable: '--font-inter'
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: 'DevFlow Next.js 13 is a clone of Stack Over flow',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
 }
 
 export default function RootLayout ({
@@ -11,9 +43,19 @@ export default function RootLayout ({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient, hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is a piece of text</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
